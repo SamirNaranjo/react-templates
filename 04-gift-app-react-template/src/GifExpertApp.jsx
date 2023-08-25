65c79da0d95c5966210785213ba41e796dc2f6c0
samir <samirnaranjo1@gmail.com>
2023-08-24 21:44:04 -0300

    Create Gift-react-app

---
+++ 04-gift-app-react-template/src/GifExpertApp.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const GifExpertApp = () => {
+  
+    return (
+
+    <h1>GifExpertApp</h1>
+
+    )
+
+}
---
+++ 04-gift-app-react-template/src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { GifExpertApp } from './GifExpertApp';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <GifExpertApp />
+  </React.StrictMode>,
+);
